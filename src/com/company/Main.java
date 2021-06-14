421b997b80ee9b2201db8e496a7f411f388d88e2
Jakub Jarząbek <jarzabek.kuba@gmail.com>
2021-06-14 21:14:58 +0200

    Zadanie 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        List<Integer> numbers = new ArrayList<>();
+        for(int i = 0; i < 100; i++){
+            numbers.add(ThreadLocalRandom.current().nextInt(0, 51));
+        }
+
+        System.out.println(numbers);
+
+        System.out.println(numbers.stream().distinct().collect(Collectors.toList()));
+
+
     }
 }
